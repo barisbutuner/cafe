32026fb95cb914ac677a26d7fd887da4a1075094
Barış Bütüner <Bar Btner@DESKTOP-5DPGQTE>
2018-01-15 00:31:02 +0300

    Add project files.

---
+++ cafe/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace cafe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.masa1 = new System.Windows.Forms.Button();
+            this.lblmasa1 = new System.Windows.Forms.Label();
+            this.txtmesaj = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblhesap = new System.Windows.Forms.Label();
+            this.btnhesapla = new System.Windows.Forms.Button();
+            this.txtsure = new System.Windows.Forms.TextBox();
+            this.txtucret = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.masa2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(37, 195);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Masa 1";
+            // 
+            // masa1
+            // 
+            this.masa1.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.masa1.Location = new System.Drawing.Point(143, 195);
+            this.masa1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.masa1.Name = "masa1";
+            this.masa1.Size = new System.Drawing.Size(100, 40);
+            this.masa1.TabIndex = 1;
+            this.masa1.Text = "Aç";
+            this.masa1.UseVisualStyleBackColor = true;
+            this.masa1.Click += new System.EventHandler(this.btnmasa1_Click);
+            // 
+            // lblmasa1
+            // 
+            this.lblmasa1.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblmasa1.Location = new System.Drawing.Point(251, 201);
+            this.lblmasa1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblmasa1.Name = "lblmasa1";
+            this.lblmasa1.Size = new System.Drawing.Size(259, 29);
+            this.lblmasa1.TabIndex = 2;
+            this.lblmasa1.Text = "Durum";
+            // 
+            // txtmesaj
+            // 
+            this.txtmesaj.Location = new System.Drawing.Point(630, 136);
+            this.txtmesaj.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtmesaj.Multiline = true;
+            this.txtmesaj.Name = "txtmesaj";
+            this.txtmesaj.Size = new System.Drawing.Size(443, 195);
+            this.txtmesaj.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblhesap);
+            this.groupBox1.Controls.Add(this.btnhesapla);
+            this.groupBox1.Controls.Add(this.txtsure);
+            this.groupBox1.Controls.Add(this.txtucret);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Georgia", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(630, 350);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.groupBox1.Size = new System.Drawing.Size(444, 246);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "İşlemler";
+            // 
+            // lblhesap
+            // 
+            this.lblhesap.AutoSize = true;
+            this.lblhesap.Location = new System.Drawing.Point(21, 182);
+            this.lblhesap.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblhesap.Name = "lblhesap";
+            this.lblhesap.Size = new System.Drawing.Size(43, 14);
+            this.lblhesap.TabIndex = 6;
+            this.lblhesap.Text = "label6";
+            // 
+            // btnhesapla
+            // 
+            this.btnhesapla.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnhesapla.Location = new System.Drawing.Point(218, 143);
+            this.btnhesapla.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnhesapla.Name = "btnhesapla";
+            this.btnhesapla.Size = new System.Drawing.Size(132, 32);
+            this.btnhesapla.TabIndex = 5;
+            this.btnhesapla.Text = "Ücret Hesapla";
+            this.btnhesapla.UseVisualStyleBackColor = true;
+            this.btnhesapla.Click += new System.EventHandler(this.btnhesapla_Click);
+            // 
+            // txtsure
+            // 
+            this.txtsure.Font = new System.Drawing.Font("Georgia", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtsure.Location = new System.Drawing.Point(218, 96);
+            this.txtsure.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtsure.Name = "txtsure";
+            this.txtsure.Size = new System.Drawing.Size(132, 29);
+            this.txtsure.TabIndex = 4;
+            // 
+            // txtucret
+            // 
+            this.txtucret.Font = new System.Drawing.Font("Georgia", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtucret.Location = new System.Drawing.Point(218, 48);
+            this.txtucret.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtucret.Name = "txtucret";
+            this.txtucret.Size = new System.Drawing.Size(132, 29);
+            this.txtucret.TabIndex = 3;
+            this.txtucret.Text = "3,50";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Georgia", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(21, 54);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(189, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Saatlik Ücret(TL):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Georgia", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(135, 99);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Süre:";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(251, 349);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(259, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Durum";
+            // 
+            // masa2
+            // 
+            this.masa2.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.masa2.Location = new System.Drawing.Point(143, 342);
+            this.masa2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.masa2.Name = "masa2";
+            this.masa2.Size = new System.Drawing.Size(100, 36);
+            this.masa2.TabIndex = 6;
+            this.masa2.Text = "Aç";
+            this.masa2.UseVisualStyleBackColor = true;
+            this.masa2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(7, 161);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(129, 111);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(7, 307);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(129, 111);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(37, 342);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Masa 2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Georgia", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(636, 110);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(135, 23);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Masa Ayrıntısı";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Harlow Solid Italic", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(225, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(505, 81);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "KAAN CAFE";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.ClientSize = new System.Drawing.Size(1075, 602);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.masa2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtmesaj);
+            this.Controls.Add(this.lblmasa1);
+            this.Controls.Add(this.masa1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Georgia", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "KAAN CAFE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button masa1;
+        private System.Windows.Forms.Label lblmasa1;
+        private System.Windows.Forms.TextBox txtmesaj;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnhesapla;
+        private System.Windows.Forms.TextBox txtsure;
+        private System.Windows.Forms.TextBox txtucret;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button masa2;
+        private System.Windows.Forms.Label lblhesap;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ cafe/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cafe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void verigetir(System.Windows.Forms.Button btn, System.Windows.Forms.Label lbl)
+        {
+            if(btn.Text=="Kapat")
+            {
+                if(txtucret.Text!="")
+                {
+                    TimeSpan d;
+                    int p;
+                    btn.Text = "Aç";
+                    d = DateTime.Now - Convert.ToDateTime(lbl.Text.Substring(25, lbl.Text.Length - 25));
+                    if ((d.Hours * 60 + d.Minutes < 60))
+                        p = 1;
+                    else
+                        p = Convert.ToInt32(Math.Round((decimal)((d.Hours * 60 + d.Minutes) / 60)));
+                    lbl.Text = "Durum : Boş, Son Oturumdan Alınan" + Convert.ToString((p * double.Parse(txtucret.Text))) + "TL";
+                    txtmesaj.Text = btn.Name + ".Masa Kapatıldı, Kapanış Zamanı :" + DateTime.Now.ToString() +
+                        "Ödenen Miktar" + Convert.ToString((p * double.Parse(txtucret.Text))) + "TL" + Environment.NewLine + txtmesaj.Text;
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen Saat Ücretini Giriniz!!!");
+                    txtucret.Focus();
+                }
+            }
+            else
+            {
+                btn.Text = "Kapat";
+                lbl.Text = "Durum: Dolu, Giriş Saati:" + DateTime.Now.ToString();
+                txtmesaj.Text = btn.Name + ".Masa Açıldı, Açılış Zamanı:" + DateTime.Now.ToString() +
+                    Environment.NewLine + txtmesaj.Text;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnmasa1_Click(object sender, EventArgs e)
+        {
+            verigetir((Button)sender, lblmasa1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            verigetir((Button)sender, label4);
+        }
+
+        private void btnhesapla_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(txtucret.Text !="")
+                {
+                    if (txtucret.Text != "")
+                    {
+                        lblhesap.Text = txtsure.Text + "Saat Kullanım Ücreti" + Convert.ToString(double.Parse(txtucret.Text) * double.Parse(txtsure.Text));
+                        
+                    }
+                    else
+                    {
+                        MessageBox.Show("Lütfen Kullanım Süresini Giriniz!!");
+                        txtsure.Focus();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Lütfen Saat Ücretini Giriniz!!");
+                    txtucret.Focus();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
